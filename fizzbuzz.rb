02b87a13f1a2c4b8f412129a71170b114ed4a9b9
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-10-08 05:35:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 require 'pry'
 
 def fizzbuzz(number)
-  if number % 5 == 0
-    "Buzz"
-  elsif
-    number % 3 == 0
+  if number % 3 == 0
     "Fizz"
+  elsif
+    number % 5 == 0
+    "Buzz"
   elsif
     number % 3 == 0 && number % 5 == 0
     "FizzBuzz"
